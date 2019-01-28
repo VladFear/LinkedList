23248b1a1f9cc053b32d9a13748ba61a546d957e
Vlad Fear <onlylastfear1@gmail.com>
2019-01-28 12:03:27 +0200

    Linked_list class was created

---
+++ list.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename T>
+class LinkedList
+{
+private:
+
+public:
+	LinkedList();
+};
+
+template <typename T>
+LinkedList<T>::LinkedList()
+{
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include "list.hpp"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
+	LinkedList<int> list;
 	return 0;
 }
