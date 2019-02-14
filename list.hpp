ab22823dba17cde3391380849f1170e0f1ca8125
Vladyslav Tsymbal <vladyslav.tsymbal@globallogic.com>
2019-02-14 16:56:39 +0200

    Size func was created

--- list.hpp
+++ list.hpp
@@ -12,11 +12,20 @@ private:
 			T data;
 	};
 
+	size_t list_size = 0;
+
 public:
 	LinkedList();
+	size_t size() const;
 };
 
 template <typename T>
 LinkedList<T>::LinkedList()
 {
+}
+
+template <typename T>
+size_t LinkedList<T>::size() const
+{
+	return list_size;
 }
